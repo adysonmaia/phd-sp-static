corrected availability calculation

analyze.py
@@ -7,9 +7,54 @@ import matplotlib.pyplot as plt
 
 
 DPI = 100
-Y_LIMITS = [-0.2, 14]
-Y_TICKS = range(0, 13, 1)
-Y_LABEL = r"QoS Violation $\varepsilon$ - ms"
+
+Y_PARAM = {
+    'max_e': {
+        'label': 'Deadline Violation - ms',
+        'limit': [0.0, 14.0]
+    },
+    'deadline_sr': {
+        'label': 'Deadline Satisfaction - %',
+        'limit': [40.0, 100.0]
+    },
+    'avg_rt': {
+        'label': 'Response Time - ms',
+        'limit': [0.0, 18.0]
+    },
+    'cost': {
+        'label': 'Cost',
+        'limit': [100.0, 1400.0]
+    },
+    'max_unavail': {
+        'label': 'Availability - %',
+        'limit': [98.0, 100.0]
+    },
+    'avg_availa': {
+        'label': 'Avg Availability - %',
+        'limit': [99.5, 100.0]
+    }
+}
+
+X_PARAM = {
+    'users': {
+        'label': 'Number of Users',
+        'limit': [-100, 100]
+    },
+    'apps': {
+        'label': 'Number of Applications',
+        'limit': [-0.5, 0.5]
+    }
+}
+
+SOL_LABEL = {
+    ('cloud', ''): 'cloud',
+    ('genetic', 'max_e'): 'so_deadline',
+    ('genetic', 'avg_rt'): 'so_rt',
+    ('genetic', 'cost'): 'so_cost',
+    ('genetic', 'max_unavail'): 'so_avail',
+    ('genetic_mo', 'v1'): 'mo_v1',
+    ('genetic_mo', 'v2'): 'mo_v2'
+}
 
 
 def get_data_from_file(filename):
@@ -24,191 +69,150 @@ def get_data_from_file(filename):
     return results
 
 
-def filter_data(data, apps=[], nodes=[], users=[], solutions=[]):
-    return list(filter(lambda row: ((not apps or int(row["apps"]) in apps)
-                                    and (not nodes or int(row["nodes"]) in nodes)
-                                    and (not users or int(row["users"]) in users)
-                                    and (not solutions or row["solution"] in solutions)),
-                       data))
+def filter_data(data, **kwargs):
+    f_values = {k: str(v) for k, v in kwargs.items()}
+
+    def in_filter(row):
+        for key, value in row.items():
+            if key in f_values and value not in f_values[key]:
+                return False
+        return True
+
+    return list(filter(lambda row: in_filter(row), data))
 
 
-def gen_apps_figure(data, solutions, apps, nb_nodes, nb_users,
-                    filename=None, nb_runs=30):
+def format_metric(value, metric):
+    value = float(value)
+    if metric == 'max_unavail':
+        value = 100.0 * (1.0 - value)
+    if metric == 'avg_availa':
+        value = 100.0 * value
+    elif metric == 'deadline_sr':
+        value = 100.0 * value
+
+    return value
+
+
+def calc_stats(values):
+    nb_runs = len(values)
+    mean = np.mean(values)
+    sem = st.sem(values)
+    if sem > 0.0:
+        # Calc confidence interval, return [mean - e, mean + e]
+        error = st.t.interval(0.95, nb_runs - 1, loc=mean, scale=sem)
+        error = error[1] - mean
+    else:
+        error = 0.0
+    return mean, error
+
+
+def gen_figure(data, solutions, metric, x, x_field, data_filter, filename=None):
     plt.clf()
     matplotlib.rcParams.update({'font.size': 17})
-    filtered = filter_data(data, nodes=[nb_nodes], users=[nb_users])
-    formats = ["-o", "-^", "--s", "-d", "--o", "-s"]
+    filtered = filter_data(data, **data_filter)
+    formats = ['-o', '-s', '-<', '-->', '-^', '-v', '-d']
     formats_len = len(formats)
     line = 0
-    x = apps
-    for solution in solutions:
-        sol_data = filter_data(filtered, solutions=[solution])
+    for solution, version in solutions:
+        sol_data = filter_data(filtered, solution=[solution], version=[version])
         y = []
         y_errors = []
-        for nb_apps in apps:
-            data = filter_data(sol_data, apps=[nb_apps])
-            values = [float(row["value"]) for row in data]
-            mean = np.mean(values)
-            sem = st.sem(values)
-            if sem > 0.0:
-                error = st.t.interval(0.95, nb_runs - 1, loc=mean, scale=sem)
-            else:
-                error = [mean, mean]
+        for i in x:
+            x_filter = {x_field: i}
+            x_data = filter_data(sol_data, **x_filter)
+            values = list(map(lambda r: format_metric(r[metric], metric), x_data))
+            mean, error = calc_stats(values)
             y.append(mean)
-            y_errors.append(error[1] - mean)
+            y_errors.append(error)
 
         line_format = formats[line % formats_len]
-        plt.errorbar(x, y, yerr=y_errors, label=solution, fmt=line_format)
+        label = SOL_LABEL[solution, version]
+        plt.errorbar(x, y, yerr=y_errors, label=label, fmt=line_format)
         line += 1
 
-    ncol = 3 if len(solutions) > 4 else 2
-    plt.legend(loc='upper left', numpoints=1, ncol=ncol, columnspacing=0.5)
-    plt.xlabel('Number of Applications')
-    plt.ylabel(Y_LABEL)
-    plt.xlim(x[0]-1, x[-1]+1)
+    # ncol = 4 if len(solutions) > 4 else 3
+    ncol = 4
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.12),
+               numpoints=1, ncol=ncol, columnspacing=0.5, fontsize=17)
+    plt.subplots_adjust(bottom=0.27, top=0.97, left=0.12, right=0.96)
+
+    x_param = X_PARAM[x_field]
+    y_param = Y_PARAM[metric]
+
+    plt.xlabel(x_param['label'])
+    plt.ylabel(y_param['label'])
+    plt.xlim(x[0] + x_param['limit'][0], x[-1] + x_param['limit'][1])
+    plt.ylim(*y_param['limit'])
     plt.xticks(x)
-    plt.ylim(*Y_LIMITS)
-    plt.yticks(Y_TICKS)
     plt.grid(True)
     if not filename:
         plt.show()
     else:
-        plt.savefig(filename, dpi=DPI, bbox_inches="tight", pad_inches=0.05)
-
-
-def gen_users_figure(data, solutions, users, nb_apps, nb_nodes,
-                     filename=None, nb_runs=30):
-    plt.clf()
-    matplotlib.rcParams.update({'font.size': 17})
-    filtered = filter_data(data, apps=[nb_apps], nodes=[nb_nodes])
-    formats = ["-o", "-^", "--s", "-d", "--o", "-s"]
-    formats_len = len(formats)
-    line = 0
-    x = users
-    for solution in solutions:
-        sol_data = filter_data(filtered, solutions=[solution])
-        y = []
-        y_errors = []
-        for nb_users in users:
-            data = filter_data(sol_data, users=[nb_users])
-            values = [float(row["value"]) for row in data]
-            mean = np.mean(values)
-            sem = st.sem(values)
-            if sem > 0.0:
-                error = st.t.interval(0.95, nb_runs - 1, loc=mean, scale=sem)
-            else:
-                error = [mean, mean]
-            y.append(mean)
-            y_errors.append(error[1] - mean)
-
-        line_format = formats[line % formats_len]
-        plt.errorbar(x, y, yerr=y_errors, label=solution, fmt=line_format)
-        line += 1
-
-    ncol = 3 if len(solutions) > 4 else 2
-    plt.legend(loc='upper left', numpoints=1, ncol=ncol, columnspacing=0.5)
-    plt.xlabel('Number of Users')
-    plt.ylabel(Y_LABEL)
-    plt.xlim(x[0]-100, x[-1]+100)
-    plt.xticks(x)
-    plt.ylim(*Y_LIMITS)
-    plt.yticks(Y_TICKS)
-    plt.grid(True)
-    if not filename:
-        plt.show()
-    else:
-        plt.savefig(filename, dpi=DPI, bbox_inches="tight", pad_inches=0.05)
-
-
-def gen_users_apps_figure(data, solutions, users, apps, nb_nodes,
-                          filename=None, nb_runs=30):
-    plt.clf()
-    filtered = filter_data(data, nodes=[nb_nodes])
-    formats = ["-o", "-^", "-s", "--o", "--^", "--s"]
-    formats_len = len(formats)
-    line = 0
-    x = users
-    for solution in solutions:
-        for nb_apps in apps:
-            sol_data = filter_data(filtered, solutions=[solution], apps=[nb_apps])
-            y = []
-            y_errors = []
-            for nb_users in users:
-                data = filter_data(sol_data, users=[nb_users])
-                values = [float(row["value"]) for row in data]
-                mean = np.mean(values)
-                sem = st.sem(values)  # Std Error Mean
-                if sem > 0.0:
-                    error = st.t.interval(0.95, nb_runs - 1, loc=mean, scale=sem)
-                else:
-                    error = [mean, mean]
-                y.append(mean)
-                y_errors.append(error[1] - mean)
-
-            line_format = formats[line % formats_len]
-            line_label = "%s (%d apps)" % (solution, nb_apps)
-            plt.errorbar(x, y, yerr=y_errors, label=line_label, fmt=line_format)
-            line += 1
-
-    plt.legend(loc='upper left', ncol=2, numpoints=1)
-    plt.xlabel('Number of Users')
-    plt.ylabel(Y_LABEL)
-    plt.xlim(x[0]-100, x[-1]+100)
-    plt.xticks(x)
-    plt.ylim(Y_LIMITS[0], 13)
-    plt.yticks(Y_TICKS)
-    plt.grid(True)
-    if not filename:
-        plt.show()
-    else:
-        plt.savefig(filename, dpi=DPI)
-
-
-def exp_2(args=[]):
-    data = get_data_from_file("output/result.csv")
-    solutions = ["greedy", "genetic", "cloud", "milp-minlp", "milp"]
-
-    users = range(1000, 10001, 3000)
-    gen_users_figure(data, solutions, users, nb_apps=30, nb_nodes=9,
-                     filename="output/exp_n9_a30_users.png")
-
-    gen_users_figure(data, solutions, users, nb_apps=20, nb_nodes=9,
-                     filename="output/exp_n9_a20_users.png")
-
-    apps = range(10, 51, 10)
-    gen_apps_figure(data, solutions, apps, nb_nodes=9, nb_users=4000,
-                    filename="output/exp_n9_u4k_apps.png")
-
-    # Part II
-    solutions = ["greedy", "genetic", "cloud", "milp-minlp-t"]
-
-    users = range(1000, 10001, 3000)
-    gen_users_figure(data, solutions, users, nb_apps=30, nb_nodes=21,
-                     filename="output/exp_n21_a30_users.png")
-
-    gen_users_figure(data, solutions, users, nb_apps=20, nb_nodes=21,
-                     filename="output/exp_n21_a20_users.png")
-
-    apps = range(10, 51, 10)
-    gen_apps_figure(data, solutions, apps, nb_nodes=21, nb_users=4000,
-                    filename="output/exp_n21_u4k_apps.png")
+        plt.savefig(filename, dpi=DPI, bbox_inches='tight', pad_inches=0.05)
 
 
 def exp_1(args=[]):
-    data = get_data_from_file("output/result.csv")
-    # solutions = ["greedy", "genetic", "cloud", "milp", "milp-minlp"]
-    solutions = ["greedy", "genetic", "cloud", "milp-minlp-t"]
+    data = get_data_from_file('output/result_exp_1.csv')
+    metric_solutions = {
+        'max_e': [
+            ('genetic_mo', 'v1'), ('genetic_mo', 'v2'),
+            ('cloud', ''),
+            ('genetic', 'max_e'),
+        ],
+        'deadline_sr': [
+            ('genetic_mo', 'v1'), ('genetic_mo', 'v2'),
+            ('cloud', ''),
+            ('genetic', 'max_e'),
+        ],
+        'avg_rt': [
+            ('genetic_mo', 'v1'), ('genetic_mo', 'v2'),
+            ('cloud', ''),
+            ('genetic', 'avg_rt'),
+        ],
+        'cost': [
+            ('genetic_mo', 'v1'), ('genetic_mo', 'v2'),
+            ('cloud', ''),
+            ('genetic', 'cost'),
+        ],
+        'max_unavail': [
+            ('genetic_mo', 'v1'), ('genetic_mo', 'v2'),
+            ('cloud', ''),
+            ('genetic', 'max_unavail'),
+        ],
+        'avg_availa': [
+            ('genetic_mo', 'v1'), ('genetic_mo', 'v2'),
+            ('cloud', ''),
+            ('genetic', 'max_unavail'),
+        ],
+    }
 
-    users = range(1000, 10001, 3000)
-    # gen_users_figure(data, solutions, users, 30, 9, filename="output/fig_users.png")
-    gen_users_figure(data, solutions, users, nb_apps=30, nb_nodes=21)
+    nodes = 27
+    r_apps = range(10, 51, 10)
+    r_users = range(1000, 10001, 3000)
 
-    # apps = range(10, 51, 10)
-    # gen_apps_figure(data, solutions, apps, nb_nodes=21, nb_users=4000)
+    params = [
+        {'field': 'apps',
+         'values': r_apps,
+         'x_values': r_users,
+         'x_field': 'users'
+         },
+        {'field': 'users',
+         'values': r_users,
+         'x_values': r_apps,
+         'x_field': 'apps'
+         },
+    ]
 
-    # users = range(1000, 10001, 3000)
-    # apps = [10, 30, 50]
-    # gen_users_apps_figure(data, solutions, users, apps, nb_nodes=9)
+    for param in params:
+        for value in param['values']:
+            for metric, solutions in metric_solutions.items():
+                data_filter = {'nodes': [nodes], param['field']: [value]}
+                x = param['x_values']
+                x_field = param['x_field']
+                filename = "output/figs/fig_{}_{}_{}.png".format(
+                    metric, param['field'], value
+                )
+                gen_figure(data, solutions, metric, x, x_field, data_filter, filename)
 
 
 if __name__ == '__main__':