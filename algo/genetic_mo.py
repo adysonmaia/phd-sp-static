04f93681ad7d4c75ff44722bc002cdc21f4209c2
Adyson Magalhães Maia <adyson.maia@gmail.com>
2019-04-08 11:14:00 +0200

    changed some genetic parameters

--- algo/genetic_mo.py
+++ algo/genetic_mo.py
@@ -45,8 +45,8 @@ class MO_Chromosome(SP_Chromosome, NSGAII_Chromosome):
 
 
 def solve(input,
-          nb_generations=200,
-          population_size=200,
+          nb_generations=100,
+          population_size=100,
           elite_proportion=0.4,
           mutant_proportion=0.3,
           dominance_error=DOMINANCE_ERROR,
